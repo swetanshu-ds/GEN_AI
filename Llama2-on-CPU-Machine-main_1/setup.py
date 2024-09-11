06acb92f2e10fb85d309797e6425f2fadd442e0c
Swetanshu Pandey <83079739+swetanshu-ds@users.noreply.github.com>
2024-09-11 08:39:36 +0530

    Update setup.py

--- Llama2-on-CPU-Machine-main_1/setup.py
+++ Llama2-on-CPU-Machine-main_1/setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy',
-    author_email= 'boktiar@ineuron.ai',
+    author= 'swetanshu',
+    author_email= 'swetanshupandey1722@gmail.com',
     packages= find_packages(),
     install_requires = []
 
